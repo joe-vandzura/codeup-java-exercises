332a159949b152c2981c7e00a8009da8abbe4029
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-02-07 11:21:07 -0600

    finished syntax, types, and variables exercises

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,5 +3,26 @@ package hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int myFavoriteNumber = 67;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string!";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        System.out.println(y);
+
+        byte maxNum = Byte.MAX_VALUE;
+        System.out.println(maxNum);
+
+
     }
 }
